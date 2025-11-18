04bb4417c86a9bffd9bea2917f164ec6007ba554
Torben <torben.soennecken@rootitup.de>
2025-11-18 11:36:41 +0100

    🐛 Fixed escalation rate and first level support solution rate

--- matrix42/resolution_rate_in_first_level_support_AND_escalation_rate.sql
+++ matrix42/resolution_rate_in_first_level_support_AND_escalation_rate.sql
@@ -9,34 +9,86 @@ WITH FirstLevelSupportTeams(RoleID) AS (
 SELECT
     ticket.ID,
     -- ticket.[Expression-ObjectID] AS ObjectID, -- Uncomment if ObjectID is needed (Warning: performance impact)
-    /* 
-     Die Bedingung ist wahr, wenn das Ticket im Status "Geschlossen" ist und
-     es keine historischen Aktionen vom Typ "Weiterleitung an Rolle" gibt,
-     welche an eine NICHT-First-Level-Support-Rolle weitergeleitet wurden. 
+    CASE
+        WHEN ticketCommon.State = 204 THEN 1
+        ELSE 0
+    END AS IsClosed,
+    CASE
+        WHEN COUNT(
+            firstLevelTicketCreationJournal.[Expression-ObjectID]
+        ) > 0 THEN 1
+        ELSE 0
+    END AS IsCreatedForFirstLevelSupport,
+    /*
+     Ein Ticket wurde im First-Level-Support gelöst, wenn:
+     - das Ticket im Status "Geschlossen" ist UND
+     - es keine historischen Aktionen vom Typ "Weiterleitung an Rolle" für eine NICHT-First-Level-Support-Rolle gibt UND
+     - die initiale Bearbeitungsrolle eine First-Level-Support-Rolle war
      */
     CASE
         WHEN ticketCommon.State = 204
-        AND COUNT(journal.[Expression-ObjectID]) = 0 THEN 1
+        AND COUNT(
+            firstLevelTicketCreationJournal.[Expression-ObjectID]
+        ) > 0
+        AND COUNT(
+            secondLevelTicketForwardJournal.[Expression-ObjectID]
+        ) = 0 THEN 1
         ELSE 0
     END AS IsFirstLevelSupportResolution,
     /*
-     Die Bedingung ist wahr, wenn es mindestens eine historische Aktion vom Typ "Weiterleitung an Rolle" gibt,
-     welche an eine NICHT-First-Level-Support-Rolle weitergeleitet wurde. 
+     Ein Ticket wurde eskaliert, wenn:
+     - es mind. eine historischen Aktionen vom Typ "Weiterleitung an Rolle" für eine NICHT-First-Level-Support-Rolle gibt UND
+     - die initiale Bearbeitungsrolle eine First-Level-Support-Rolle war
      */
     CASE
-        WHEN COUNT(journal.[Expression-ObjectID]) > 0 THEN 1
+        WHEN COUNT(
+            secondLevelTicketForwardJournal.[Expression-ObjectID]
+        ) > 0
+        AND COUNT(
+            firstLevelTicketCreationJournal.[Expression-ObjectID]
+        ) > 0 THEN 1
         ELSE 0
     END AS IsEscalated
 FROM
     dbo.SPSActivityClassBase AS ticket
-    LEFT OUTER JOIN (
+    /*
+     Verknüpfung mit den Ticket-Metadaten, um den aktuellen Status zu erhalten.
+     */
+    INNER JOIN (
         SELECT
             [Expression-ObjectID],
             State
         FROM
             dbo.SPSCommonClassBase
     ) AS ticketCommon ON ticket.[Expression-ObjectID] = ticketCommon.[Expression-ObjectID]
-    /* Wir erzeugen Zeilen, in welcher entweder (NULL) oder die KPI verletzende Weiterleitungs-Aktion vorkommt. */
+    /* 
+     Verknüpfung mit den Journal-Einträgen, um die Ticket-Erstellungs-Aktion zu identifizieren.
+     Ziel ist es zu prüfen, ob das Ticket ursprünglich für den First-Level-Support erstellt wurde.
+     */
+    LEFT OUTER JOIN (
+        SELECT
+            [Expression-ObjectID]
+        FROM
+            dbo.SPSActivityClassUnitOfWork
+        WHERE
+            /* Nur Journal-Einträge für "Erstellt"" */
+            ActivityAction = 1
+            AND
+            /* Nur Fragmente, WELCHE der SPSScRoleClassBase einer First-Level-Support-Rolle entsprechen */
+            CAST(SolutionParams AS XML).value(
+                '(/parameters/JournalEntryParameterBase/FragmentIds/fragmentId)[1]',
+                'uniqueidentifier'
+            ) IN (
+                SELECT
+                    RoleID
+                FROM
+                    FirstLevelSupportTeams
+            )
+    ) AS firstLevelTicketCreationJournal ON ticketCommon.[Expression-ObjectID] = firstLevelTicketCreationJournal.[Expression-ObjectID]
+    /*
+     Verknüpfung mit den Journal-Einträgen, um Weiterleitungs-Aktionen zu identifizieren.
+     Ziel ist es zu prüfen, ob das Ticket an eine NICHT-First-Level-Support-Rolle weitergeleitet (eskaliert) wurde.
+     */
     LEFT OUTER JOIN (
         SELECT
             [Expression-ObjectID]
@@ -56,7 +108,7 @@ FROM
                 FROM
                     FirstLevelSupportTeams
             )
-    ) AS journal ON ticketCommon.[Expression-ObjectID] = journal.[Expression-ObjectID]
+    ) AS secondLevelTicketForwardJournal ON ticketCommon.[Expression-ObjectID] = secondLevelTicketForwardJournal.[Expression-ObjectID]
 WHERE
     ticket.CreatedDate >= DATEADD(year, -3, GETDATE()) -- 3 year sliding window
 GROUP BY
